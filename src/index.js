22d66728eec6571f5b9729cdaebe4a33549efc9c
surendranath-avvaru <surendranath.avvaru@aspiresys.com>
2018-03-03 23:58:32 +0530

    added till 026-listing video items-disp thumbnails n titles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDAO-plFGVb_kiv4bo0hkQFT46tmKVmJzI';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos : [] };
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
